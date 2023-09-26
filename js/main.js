b65e22d6f35e90581151e3691dde7c3f129fd41b
LexusLFA220 <alex.amin@lc.cuny.edu>
2023-09-26 16:49:48 -0400

    Created turns and marking X and O on board.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+/*----- functions -----*/
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+
+    console.log(board);
+    render();
+    };
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    render();
+
+    };
+    
+    //be sure to call the init function!
+    function render() {
+        board.forEach(function(mark, index){
+
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
+
+        console.log(mark, index);
+        });
+        messages.textContent = `It's ${turn}'s turn!`;
+        };
+    init();
