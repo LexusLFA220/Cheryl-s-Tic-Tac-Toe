5c70f58ef1a381858d1f1b96712858285ce79929
LexusLFA220 <alex.amin@lc.cuny.edu>
2023-09-26 18:55:07 -0400

    Reformat Code and Tie working

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 /*----- constants -----*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
 let win;
-win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
-<condition1 && condition2 && condition3 > ? <if all 3 conditions are true, this> : <else, this></else>
+
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+
 /*----- functions -----*/
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
+};
 function handleTurn(event) {
     let idx = squares.findIndex(function(square) {
     return square === event.target;
     });
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
-
-    console.log(board);
+    win = getWinner();
     render();
     };
 
@@ -40,8 +55,8 @@ function init() {
         //this sets the text content of the square of the same position to the mark on the board. 
         squares[index].textContent = mark;
 
-        console.log(mark, index);
+        
         });
-        messages.textContent = `It's ${turn}'s turn!`;
+        messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
         };
     init();
